a8e3ea47f5f94bbe8ed86442cc197ff1100714a1
Giova <giovanotti1906@hotmail.com>
2020-10-13 21:39:50 -0500

    App finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
+import Frase from './components/Frase';
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007D35 0%, #007D35 40%, #0F574E 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  cursor: pointer;
+`;
 
 function App() {
+
+  const [frase, setFrase] = useState({});
+
+  /*
+  const consultarAPI = () => {
+    const api = fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = api.then(respuesta => respuesta.json());
+    frase.then(resultado => console.log(resultado));
+  }
+  */
+
+  // Hacemos lo de ARRIBA pero con async y await
+  const consultarAPI = async () => {
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = await api.json();
+    setFrase(frase[0]);
+  }
+
+  useEffect(() => {
+    consultarAPI();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Frase
+        frase = { frase }
+      />
+      <Boton
+        onClick = { consultarAPI }
+      >
+        Obtener Frase
+      </Boton>
+    </Contenedor>
   );
 }
 
